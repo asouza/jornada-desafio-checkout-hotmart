03c50f7a4843b98044ebc101752a535cbbf09d04
Alberto <alberto.tavares@zup.com.br>
2023-11-16 15:49:52 -0300

    novo projeto

---
+++ src/main/java/com/deveficiente/desafiocheckouthotmart/DesafiocheckouthotmartApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiocheckouthotmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocheckouthotmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocheckouthotmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveficiente/desafiocheckouthotmart/DesafiocheckouthotmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiocheckouthotmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocheckouthotmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
