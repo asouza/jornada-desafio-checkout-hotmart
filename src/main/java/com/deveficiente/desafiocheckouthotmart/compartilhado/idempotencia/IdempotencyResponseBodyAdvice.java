445e5f769869d411f08de27eba73b4cfff0cc246
Alberto <alberto.tavares@zup.com.br>
2023-12-10 19:06:53 -0300

    feature(idempotencia) wip: o interceptor em si nao estava servindo para nada. Precisa achar outra alternativa

--- src/main/java/com/deveficiente/desafiocheckouthotmart/compartilhado/idempotencia/IdempotencyResponseBodyAdvice.java
+++ src/main/java/com/deveficiente/desafiocheckouthotmart/compartilhado/idempotencia/IdempotencyResponseBodyAdvice.java
@@ -1,25 +1,55 @@
 package com.deveficiente.desafiocheckouthotmart.compartilhado.idempotencia;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import com.deveficiente.desafiocheckouthotmart.compartilhado.Log5WBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class IdempotencyResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+	
+	@Autowired
+	private HttpServletRequest httpServletRequest;
+	
+	
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(IdempotencyResponseBodyAdvice.class);
+
 
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
-        // Decida se o conselho deve ser aplicado com base no tipo de retorno e/ou no conversor
-        return true; // Retornar true significa que o conselho será aplicado
+
+        boolean isAPost = returnType.hasMethodAnnotation(PostMapping.class);
+        Optional<String> needsIdempotency = Optional.ofNullable(httpServletRequest.getHeader("Idempotency-Key"));        
+        
+        Log5WBuilder	
+        	.metodo()
+        	.oQueEstaAcontecendo("Analyzing if it should apply the IdempotencyResponseAdvice")
+        	.adicionaInformacao("isAPost", isAPost+"")
+        	.adicionaInformacao("needsIdempotency", needsIdempotency+"")
+        	.info(log);
+        
+        
+		return isAPost && needsIdempotency.isPresent();
     }
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
                                   Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
 
-    	
+    	String idempotencyKey = httpServletRequest.getHeader("Idempotency-Key");
     	
     	
         return body; // Retornar o corpo da resposta modificado ou inalterado
