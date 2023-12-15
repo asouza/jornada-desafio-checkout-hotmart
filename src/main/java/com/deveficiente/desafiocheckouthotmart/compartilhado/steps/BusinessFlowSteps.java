5a93960a4988b96d62888bfd052e21628d059179
Alberto <alberto.tavares@zup.com.br>
2023-12-15 18:01:47 -0300

    feat(Steps) wip: logando quando retorna execucao previa

--- src/main/java/com/deveficiente/desafiocheckouthotmart/compartilhado/steps/BusinessFlowSteps.java
+++ src/main/java/com/deveficiente/desafiocheckouthotmart/compartilhado/steps/BusinessFlowSteps.java
@@ -2,7 +2,11 @@ package com.deveficiente.desafiocheckouthotmart.compartilhado.steps;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.support.TransactionTemplate;
+
+import com.deveficiente.desafiocheckouthotmart.compartilhado.Log5WBuilder;
 
 import io.netty.util.internal.shaded.org.jctools.queues.MessagePassingQueue.Supplier;
 
@@ -11,6 +15,11 @@ public class BusinessFlowSteps {
 	private BusinessFlowEntity currentFlow;
 	private TransactionTemplate transactionTemplate;
 	private BusinessFlowRepository businessFlowRepository;
+	
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(BusinessFlowSteps.class);
+
 
 	public BusinessFlowSteps(BusinessFlowEntity currentFlow,
 			TransactionTemplate transactionTemplate,
@@ -29,6 +38,12 @@ public class BusinessFlowSteps {
 		return
 
 		possibleStep.map(step -> {
+			Log5WBuilder
+				.metodo("executeOnlyOnce")
+				.oQueEstaAcontecendo("Returning previous execution")
+				.adicionaInformacao("stepName", stepName)
+				.info(log);
+			
 			return step.getExecutionResult();
 		}).orElseGet(() -> {
 			return transactionTemplate.execute(status -> {
