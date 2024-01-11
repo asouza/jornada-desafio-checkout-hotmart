0617d5a2c782a8654e52c629622caafb155e0bab
Alberto <alberto.tavares@zup.com.br>
2024-01-11 17:59:17 -0300

    chore(Provisionamento) rodando o agendamento a cada hora

--- src/main/java/com/deveficiente/desafiocheckouthotmart/checkout/CalculaValoresAReceberPorCompra.java
+++ src/main/java/com/deveficiente/desafiocheckouthotmart/checkout/CalculaValoresAReceberPorCompra.java
@@ -36,7 +36,7 @@ public class CalculaValoresAReceberPorCompra {
 			.getLogger(CalculaValoresAReceberPorCompra.class);
 
 	@GetMapping("/879r4hfkr89y4i4gkuhg34iuygg")
-	@Scheduled(fixedRate = 5000) // Executa a cada 5 segundos
+	@Scheduled(fixedRate = 3600)
 	public void calcula() {
 		
 		Log5WBuilder
